fa4169f04e9c81f57301e6c27319f7f554194f6f
okabeko <2minus.plus2@gmail.com>
2018-05-25 10:11:54 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
   
   # ７つの基本ルーティングをこれで省略してる
   resources :tasks
+  
+  # ユーザ登録用のルーティング。edit,update,destroyを除く
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create ]
+  
 end
